677a0ba2f212e0039ee5c9b304e27771b9c02b86
Ramiz khan <>
2018-09-25 22:27:05 -0500

    login stuff half done

--- theRec/src/app/app.module.ts
+++ theRec/src/app/app.module.ts
@@ -9,13 +9,19 @@ import { HomePage } from '../pages/home/home';
 import { GamesPage } from '../pages/games/games';
 import { TournamentsPage } from '../pages/tournaments/tournaments';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { firebaseConfig } from '../theme/config';
+
 @NgModule({
   declarations: [
     MyApp,
+    LoginPage,
     ScoresPage,
     GamesPage,
     ContactPage,
@@ -25,11 +31,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig.fire),
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    LoginPage,
     ScoresPage,
     GamesPage,
     ContactPage,
@@ -40,7 +49,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AngularFireAuth
   ]
 })
 export class AppModule {}
---
+++ theRec/src/theme/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    fire: {
+        apiKey: "AIzaSyDDOvdHl7tmucnDdFQFRadZoSVaH9szvlE",
+        authDomain: "activity-on-demand.firebaseapp.com",
+        databaseURL: "https://activity-on-demand.firebaseio.com",
+        projectId: "activity-on-demand",
+        storageBucket: "activity-on-demand.appspot.com",
+        messagingSenderId: "58497548688"
+    }
+};
